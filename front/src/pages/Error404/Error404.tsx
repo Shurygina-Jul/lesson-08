914b24527a120f69dd047feb9ac9fe729388ba05
Shurygina-Jul <melam.2916@gmail.com>
2021-04-16 00:01:51 +0300

    start Error 404

--- front/src/pages/Error404/Error404.tsx
+++ front/src/pages/Error404/Error404.tsx
@@ -12,9 +12,21 @@ const b = block('error404')
 export const Error404: React.FC<Props> = () => {
   return (
     <div className={b()}>
-      <h1 className={b('message')}>
+      <div>
+  <aside><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4424790/Mirror.png" alt="404 Image" />
+  </aside>
+  <main>
+    <h1>Sorry!</h1>
+    <p>
+      Either you aren't cool enough to visit this page or it doesn't exist <em>. . . like your social life.</em>
+    </p>
+   </main>
+</div>
+
+
+      {/* <h1 className={b('message')}>
         Ошибка 404, страница не найдена
-      </h1>
+      </h1> */}
     </div>
   )
 }
